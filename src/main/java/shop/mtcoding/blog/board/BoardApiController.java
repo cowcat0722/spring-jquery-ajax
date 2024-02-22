3e5747662d22945acd4351db5f74ad353851136e
Pakchan <1625787@donga.ac.kr>
2024-02-22 10:55:42 +0900

    ãjQuery 코드 작성

--- src/main/java/shop/mtcoding/blog/board/BoardApiController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardApiController.java
@@ -1,5 +1,6 @@
 package shop.mtcoding.blog.board;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,8 @@ public class BoardApiController {
     private final BoardRepository boardRepository;
 
     @GetMapping("/api/boards")
-    public ApiUtil<?> findAll() {
+    public ApiUtil<?> findAll(HttpServletResponse response) {
+//        response.setStatus(401); 테스트 해봤음
         List<Board> boardList = boardRepository.selectAll();
         return new ApiUtil<>(boardList); // MessageConverter
     }
