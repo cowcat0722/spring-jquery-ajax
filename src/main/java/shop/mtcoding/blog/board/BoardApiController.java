d539397aadbbbff7c83e6a41708fcacdf51057e7
Pakchan <1625787@donga.ac.kr>
2024-02-22 12:21:00 +0900

    BoardApiController, ApiUtil, jQery, FontAwesome

---
+++ src/main/java/shop/mtcoding/blog/board/BoardApiController.java
@@ -0,0 +1,19 @@
+package shop.mtcoding.blog.board;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class BoardApiController {
+    private final BoardRepository boardRepository;
+
+    @GetMapping("/api/boards")
+    public ApiUtil<?> findAll(){
+        List<Board> boardList = boardRepository.selectAll();
+        return new ApiUtil<>(boardList);
+    }
+}
